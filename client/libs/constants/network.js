52b360a609c22aaef5bb3bedd06e320fa15942a8
Seth Bertlshofer <seth@builtbyhq.com>
2017-08-18 15:36:07 -0600

    removed client

--- client/libs/constants/network.js
+++
@@ -1,9 +0,0 @@
-export default {
-  DOMAIN    : 'https://sibi-development.herokuapp.com',
-  GET       : 'get',
-  POST      : 'post',
-  PUT       : 'put',
-  DEL       : 'delete',
-  PATCH     : 'patch',
-  TIMEOUT   : 20000,
-};
