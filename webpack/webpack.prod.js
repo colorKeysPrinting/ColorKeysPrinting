90fb2b90d0a242b97a3a40cd60486f56d89bc6b3
Seth Bertlshofer <seth@builtbyhq.com>
2017-08-21 09:09:38 -0600

    updated s3 url

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -4,5 +4,5 @@ module.exports = WebpackConfig({
     env             : 'production',
     APP_NAME        : 'sibi_ge_admin',
     ASSETS_PORT     : 8080,
-    HOST            : '<s3 url>'
+    HOST            : ' http://sibi-ge-admin.s3-website-us-west-2.amazonaws.com'
 });
