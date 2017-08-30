7f80b7821d8aebf73f3baef34fdfaee75f17e6c0
Seth Bertlshofer <seth@builtbyhq.com>
2017-08-29 18:25:16 -0600

    updates to process_order_page for functionality

--- src/screens/process_order_page.jsx
+++ src/screens/process_order_page.jsx
@@ -9,7 +9,6 @@ import assets                                               from 'libs/assets';
 import { logout }                                           from 'ducks/active_user/actions';
 import { getOrderById, approveOrder, getProducts, getOrder } from 'ducks/orders/actions';
 import { getUsers }                                         from 'ducks/users/actions';
-import { setActiveTab }                                     from 'ducks/header/actions';
 
 import MyTable                                              from 'components/my_table';
 
@@ -17,25 +16,39 @@ class ProcessOrderPage extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = { orderNumber: '', processedBy: '' };
+
+        this.update = this.update.bind(this);
+        this.processOrder = this.processOrder.bind(this);
     }
 
     componentWillMount() {
-        const { cookies } = this.props;
-        const jwt = cookies.get('sibi-admin-jwt');
-        const orderId = '3206bf49-9fc6-437e-b867-063ada21f005';
+        const reOrder = /orderId=(.*)/;
 
-        if (jwt && orderId) {
-            this.props.getOrderById({ token: jwt.token, id: orderId });
-            this.props.getUsers({ token: jwt.token });
-        } else {
-            console.log('TODO: trigger logout function *** no JWT ***');
+        const orderId = reOrder.exec(this.props.location.search)[1];
+
+        if (orderId) {
+            this.props.getOrderById({ id: orderId });
         }
 
-        this.props.setActiveTab('orders');
+    }
+
+    update({ type, value}) {
+        this.setState({ [type]: value });
+    }
+
+    processOrder() {
+        console.log('process order click')
     }
 
     render() {
-        let userData, occupancyData, officeData;
+        let detailsHeaderSection,
+            userData,
+            occupancyData,
+            officeData,
+            productsColGroup,
+            productData,
+            orderTotalSection;
 
         const { cookies } = this.props;
         const jwt = cookies.get('sibi-admin-jwt');
@@ -62,14 +75,57 @@ class ProcessOrderPage extends React.Component {
             email: 'Email'
         };
 
-        if (this.props.order.size > 0 && this.props.users.size > 0) {
-            console.log("GOT USERS AND ORDER");
-            const order = this.props.order.toJS();
-            const user = _.find(this.props.users.toJS(), ['id', order.userId]);
-            console.log(order);
-            console.log(user);
+        const productHeaders = {
+            productImage: 'Product',
+            productDescription: '',
+            model: 'Model # or Code #',
+            qty: 'Qty',
+            price: 'Cost'
+        };
 
-            //USER TABLE DATA
+        if (this.props.order.size > 0) {
+
+            const order = this.props.order.toJS();
+            const user = order.createdByUser;
+
+            const orderProcessHeading = {
+                address: `${order.fundProperty.addressLineOne} ${order.fundProperty.addressLineTwo} ${order.fundProperty.addressLineThree}, ${order.fundProperty.city}, ${order.fundProperty.state}, ${order.fundProperty.zipcode}`,
+                fund: 'TODO: get fund name',
+                accountNumber: (order.pmOffice.applianceGEAccountNumber) ? order.pmOffice.applianceGEAccountNumber : 'TODO: get fund account number'
+            };
+
+            detailsHeaderSection = <div className="details-header">
+                <div className="header-property pure-u-2-3">
+                    <div>
+                        <h2>Account #:</h2>
+                        <div>{ orderProcessHeading.accountNumber }</div>
+                    </div>
+                    <div>
+                        <h2>Fund:</h2>
+                        <div>{ orderProcessHeading.fund.name }</div>
+                    </div>
+                    <div>
+                        <h3>Ship-to Address:</h3>
+                        <div>{ orderProcessHeading.address }</div>
+                    </div>
+                </div>
+                <div className="button-container pure-u-1-3">
+                    <form onSubmit={this.processOrder}>
+                        <div>
+                            <label htmlFor="processed-by">Processed By</label>
+                            <input name="processed-by" type="text" value={this.state.processedBy} placeholder="Jane Doe" onChange={(e) => this.update({ type: 'processedBy', value: e.target.value })} required />
+                        </div>
+                        <div>
+                            <label htmlFor="ge-order-number">GE Order Number</label>
+                            <input name="ge-order-number" type="number" value={this.state.orderNumber} placeholder="Jane Doe" onChange={(e) => this.update({ type: 'orderNumber', value: e.target.value })} required />
+                        </div>
+
+                        <input className = "btn submit-btn fill" type="submit" value="Process Order" />
+                    </form>
+                </div>
+            </div>;
+
+            // ***************** USER TABLE DATA *****************
             const userCols = {};
             _.each(userHeaders, (value, key) => {
                 value = order[key]
@@ -83,19 +139,19 @@ class ProcessOrderPage extends React.Component {
                     value = user.email;
 
                 } else if (key === 'hotshotDelivery') {
-                    value = "Yes";
+                    value = 'Yes';
 
                 } else if (key === 'hotshotInstallDate') {
                     value = dateformat(order.installDate, 'mm/dd/yy');
 
                 } else if (key === 'hotshotCode') {
-                    value = "NEED HOTSHOT CODE";
+                    value = 'NEED HOTSHOT CODE';
                 }
                 userCols[key] = value;
             });
-            userData = {userCols};
+            userData = { userCols };
 
-            //OCCUPANCY TABLE DATA
+            // ***************** OCCUPANCY TABLE DATA *****************
             const occupancyCols = {};
             _.each(occupancyHeaders, (value, key) => {
                 value = order[key]
@@ -104,18 +160,18 @@ class ProcessOrderPage extends React.Component {
 
                 } else if (key === 'tenantName'){
                     value = order.tenantFirstName + ' ' + order.tenantLastName;
-                
+
                 } else if (key === 'phoneNumber') {
                     value = order.tenantPhone;
 
                 } else if (key === 'email') {
                     value = order.tenantEmail;
-                } 
+                }
                 occupancyCols[key] = value;
             });
             occupancyData = {occupancyCols};
 
-            //OFFICE TABLE DATA
+            // ***************** OFFICE TABLE DATA *****************
             const officeCols = {};
             _.each(officeHeaders, (value, key) => {
                 value = order[key]
@@ -124,36 +180,129 @@ class ProcessOrderPage extends React.Component {
 
                 } else if (key === 'phoneNumber'){
                     value = order.pmOffice.phoneNumber;
-                
+
                 } else if (key === 'email') {
                     value = 'NEED PM OFFICE EMAIL';
                 }
                 officeCols[key] = value;
             });
             officeData = {officeCols};
+
+            // ***************** PRODUCTS TABLE DATA *****************
+            const productHeaderHTML = _.map(productHeaders, (header, key) => {
+                if (key !== 'productImage') {
+                    return <td>{ header }</td>
+                }
+            });
+
+            productData = _.map(order.productsAndDestinations, (orderDetail) => {
+                const detail = orderDetail.product;
+                let productImage, productDetails;
+
+                _.each(productHeaders, (value, key) => {
+                    if (key === 'productImage') {
+                        productImage = <table>
+                            <thead className="head">
+                                <tr className="table-row"><td>{ value }</td></tr>
+                            </thead>
+                            <tbody>
+                                <tr className="table-row"><td><div><img src={detail.applianceColorsAndImages[0].imageUrl} alt="productImg" width="200" height="auto" /></div></td></tr>
+                            </tbody>
+                        </table>;
+
+                    } else {
+                        productDetails = <table>
+                            <colgroup>
+                                <col span="1" style={{ width: '45%'}} />
+                                <col span="1" style={{ width: '35%'}} />
+                                <col span="1" style={{ width: '10%'}} />
+                                <col span="1" style={{ width: '10%'}} />
+                            </colgroup>
+                            <thead className="head">
+                                <tr className="table-row">
+                                    { productHeaderHTML }
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="table-row">
+                                    <td className="table-cell-title">{ detail.applianceDescription }</td>
+                                    <td className="table-cell-details">Model #{ detail.sibiModelNumber }</td>
+                                    <td className="table-cell-details">{ orderDetail.qty }</td>
+                                    <td className="table-cell-details">${ orderDetail.ProductPrice.price }</td>
+                                </tr>
+                                <tr className="table-row"><td className="table-cell-details"><div className="btn submit-btn" onClick={() => console.log('change product model #')}>Out of Stock?</div></td></tr>
+                                <tr className="table-row">
+                                    <td className="table-cell-details">Install Description: { detail.applianceInstallDescription }</td>
+                                    <td className="table-cell-details">Install Code #{ detail.applianceInstallCode }</td>
+                                    <td className="table-cell-details"></td>
+                                    <td className="table-cell-details">${ detail.applianceInstallPrice }</td>
+                                </tr>
+                                <tr className="table-row">
+                                    <td className="table-cell-details">Remove Appliance Description: { detail.applianceRemovalDescription }</td>
+                                    <td className="table-cell-details">Remove Code #{ detail.applianceRemovalDescription }</td>
+                                    <td className="table-cell-details"></td>
+                                    <td className="table-cell-details">${ detail.applianceRemovalPrice }</td>
+                                </tr>
+                                <tr className="table-row">
+                                    <td className="table-cell-details">Disconnect Fee: </td>
+                                    <td className="table-cell-details">Disconnect Code #{  }</td>
+                                    <td className="table-cell-details"></td>
+                                    <td className="table-cell-details">${  }</td>
+                                </tr>
+                            </tbody>
+                        </table>;
+                    }
+                })
+                return (
+                    <tr key={`orderDetails${orderDetail.fundPropertyId}`} className="table-row">
+                        <td>{ productImage }</td>
+                        <td>{ productDetails }</td>
+                    </tr>
+                );
+            });
+
+            orderTotalSection = <div className="cost-section">
+                <h4>Order Summary </h4>
+                <div className="cost-row" >
+                    <div><h4>Sub Total:</h4> ${ order.totalCost }</div>
+                    <div><h4>Sales Tax:</h4> ${ order.salesTax }</div>
+                    <div><h4>Total:</h4> ${ parseFloat(order.totalCost) + parseFloat(order.salesTax) }</div>
+                </div>
+            </div>;
         }
 
         return (
             <div id="process-orders-page">
                 <div className="container">
+                    { detailsHeaderSection }
                     <MyTable
-                        tableClass="user-table"
+                        className="user-table"
                         type="userDetails"
                         headers={userHeaders}
                         data={userData}
                     />
                     <MyTable
-                        tableClass="occupancy-table"
+                        className="occupancy-table"
                         type="occupancyDetails"
                         headers={occupancyHeaders}
                         data={occupancyData}
                     />
                     <MyTable
-                        tableClass="office-table"
+                        className="office-table"
                         type="officeDetails"
                         headers={officeHeaders}
                         data={officeData}
                     />
+                    <table className="table" >
+                        <colgroup>
+                            <col span="1" style={{ width: '10%'}} />
+                            <col span="1" style={{ width: '90%'}} />
+                        </colgroup>
+                        <tbody>
+                            { productData }
+                        </tbody>
+                    </table>
+                    { orderTotalSection }
                 </div>
             </div>
         );
@@ -171,8 +320,7 @@ const actions = {
     approveOrder,
     getOrderById,
     getOrder,
-    getUsers,
-    setActiveTab
+    getUsers
 }
 
 export default connect(select, actions, null, { withRef: true })(withRouter(withCookies(ProcessOrderPage)));
