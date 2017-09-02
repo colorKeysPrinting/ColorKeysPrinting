f570a956e9c198f4f947885b71358609480a1e9e
Seth Bertlshofer <seth@builtbyhq.com>
2017-09-02 00:50:02 -0600

    added edit order iFrame

--- src/screens/order_details.jsx
+++ src/screens/order_details.jsx
@@ -5,6 +5,7 @@ import { withRouter }                                       from 'react-router';
 import { withCookies }                                      from 'react-cookie';
 import dateformat                                           from 'dateformat';
 import assets                                               from 'libs/assets';
+import Iframe                                               from 'react-iframe';
 
 import { logout }                                           from 'ducks/active_user/actions';
 import { getOrderById, approveOrder, getProducts }          from 'ducks/orders/actions';
@@ -18,8 +19,9 @@ class OrderDetails extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { productsAndParts: '' };
+        this.state = { productsAndParts: '', editOrder: false };
 
+        this.editOrder = this.editOrder.bind(this);
         this.handleAction = this.handleAction.bind(this);
     }
 
@@ -61,6 +63,10 @@ class OrderDetails extends React.Component {
         this.setState({ productsAndParts: '' });
     }
 
+    editOrder({ orderId }) {
+        this.setState({ editOrder: orderId });
+    }
+
     handleAction({orderId}) {
         console.log('user action:', orderId);
         const { cookies } = this.props;
@@ -97,159 +103,168 @@ class OrderDetails extends React.Component {
             const order = this.props.order.toJS();
             const user = order.orderUser;
 
-            const orderId = order.id;
-            const orderStatus = order.orderStatus;
-            const orderPageHeading = {
-                address: `${order.fundProperty.addressLineOne} ${order.fundProperty.addressLineTwo} ${order.fundProperty.addressLineThree}, ${order.fundProperty.city}, ${order.fundProperty.state}, ${order.fundProperty.zipcode}`,
-                PM: order.fund.pmOffices[0].name,
-                orderNumber: order.orderNumber
-            };
+            if (!this.state.editOrder) {
+                const orderId = order.id;
+                const orderStatus = order.orderStatus;
+                const orderPageHeading = {
+                    address: `${order.fundProperty.addressLineOne} ${order.fundProperty.addressLineTwo} ${order.fundProperty.addressLineThree}, ${order.fundProperty.city}, ${order.fundProperty.state}, ${order.fundProperty.zipcode}`,
+                    PM: order.fund.pmOffices[0].name,
+                    orderNumber: order.orderNumber
+                };
 
-            const tenantInfo = {
-                tenantName: `${order.tenantFirstName} ${order.tenantLastName}`,
-                tenantPhoneNumber: order.tenantPhone,
-                tenantEmail: order.tenantEmail
-            }
-
-            orderHeaders['lockBoxCode'] = (order.lockBoxCode) ? order.lockBoxCode : 'Tenant';
-
-            const cols = {};
-            _.each(orderHeaders, (value, key) => {
-                value = order[key]
-                if (key === 'orderStatus') {
-                    value = order.orderStatus;
-
-                } else if (key === 'orderNumber'){
-                    value = order.orderNumber;
-
-                } else if (key === 'deliveryDate') {
-                    value = dateformat(order.installDate, 'mm/dd/yy');
-
-                } else if (key === 'installTime') {
-                    value = order.applianceDeliveryTime ? order.applianceDeliveryTime : 'Not Specified';
-
-                } else if (key === 'occupied') {
-                    value = (order['occupied'] === false) ? 'Unoccupied' : 'Occupied';
-
-                } else if (key === 'lockBoxCode') {
-                    value = (order.lockBoxCode) ? order.lockBoxCode : `${order.tenantFirstName} ${order.tenantLastName}`;
-
-                } else if (key === 'createdBy') {
-                    value = order.createdBy ? order.createdBy : 'Ordered By';
+                const tenantInfo = {
+                    tenantName: `${order.tenantFirstName} ${order.tenantLastName}`,
+                    tenantPhoneNumber: order.tenantPhone,
+                    tenantEmail: order.tenantEmail
                 }
 
-                cols[key] = value;
-            });
-            const orderData = {cols};
+                orderHeaders['lockBoxCode'] = (order.lockBoxCode) ? order.lockBoxCode : 'Tenant';
 
-            const productData = _.map(this.state.productsAndParts, (orderDetail, productIndex) => {
-                if (orderDetail.product) {
-                    const address = <div className="no-limit">
-                        <div>{`${order.fundProperty.addressLineOne} ${order.fundProperty.addressLineTwo} ${order.fundProperty.addressLineThree},`}</div>
-                        <div>{`${order.fundProperty.city}, ${order.fundProperty.state}, ${order.fundProperty.zipcode}`}</div>
-                    </div>;
+                const cols = {};
+                _.each(orderHeaders, (value, key) => {
+                    value = order[key]
+                    if (key === 'orderStatus') {
+                        value = order.orderStatus;
 
-                    return <ProductTable
-                        key={`product${productIndex}`}
-                        type="orderDetails"
-                        productIndex={productIndex}
-                        productHeaders={productHeaders}
-                        product={orderDetail.product}
-                        image={orderDetail.selectedColorInfo.imageUrl}
-                        color={orderDetail.selectedColorInfo.color}
-                        qty={(orderDetail.qty) ? orderDetail.qty : 1}
-                        address={address}
-                        price={orderDetail.ProductPrice.price}
-                    />;
-                } else if (orderDetail.part) {
-                    return <PartTable
-                        key={`part${productIndex}`}
-                        type="orderDetails"
-                        productIndex={productIndex}
-                        part={orderDetail.part}
-                        qty={(orderDetail.qty) ? orderDetail.qty : 1}
-                        price={orderDetail.PartPrice.price}
-                    />;
-                }
-            });
+                    } else if (key === 'orderNumber'){
+                        value = order.orderNumber;
 
-            const buttonSection = (orderStatus == 'Pending') ? <div className="button-container pure-u-1-3">
-                {/* <div className="btn blue">Edit</div> */}
-                <div className="btn blue" onClick={() => this.handleAction({orderId})}>Approve</div>
-            </div>
-                : <div className="button-container pure-u-1-3"></div>;
+                    } else if (key === 'deliveryDate') {
+                        value = dateformat(order.installDate, 'mm/dd/yy');
 
-            const detailsHeaderSection = <div className="details-header">
-                <div className="header-property pure-u-2-3">
-                    <h2 className="property-address">{orderPageHeading.orderNumber}</h2>
-                    <div className="property-manager">{orderPageHeading.address} ● PM Office: {orderPageHeading.PM}</div>
+                    } else if (key === 'installTime') {
+                        value = order.applianceDeliveryTime ? order.applianceDeliveryTime : 'Not Specified';
+
+                    } else if (key === 'occupied') {
+                        value = (order['occupied'] === false) ? 'Unoccupied' : 'Occupied';
+
+                    } else if (key === 'lockBoxCode') {
+                        value = (order.lockBoxCode) ? order.lockBoxCode : `${order.tenantFirstName} ${order.tenantLastName}`;
+
+                    } else if (key === 'createdBy') {
+                        value = order.createdBy ? order.createdBy : 'Ordered By';
+                    }
+
+                    cols[key] = value;
+                });
+                const orderData = {cols};
+
+                const productData = _.map(this.state.productsAndParts, (orderDetail, productIndex) => {
+                    if (orderDetail.product) {
+                        const address = <div className="no-limit">
+                            <div>{`${order.fundProperty.addressLineOne} ${order.fundProperty.addressLineTwo} ${order.fundProperty.addressLineThree},`}</div>
+                            <div>{`${order.fundProperty.city}, ${order.fundProperty.state}, ${order.fundProperty.zipcode}`}</div>
+                        </div>;
+
+                        return <ProductTable
+                            key={`product${productIndex}`}
+                            type="orderDetails"
+                            productIndex={productIndex}
+                            productHeaders={productHeaders}
+                            product={orderDetail.product}
+                            image={orderDetail.selectedColorInfo.imageUrl}
+                            color={orderDetail.selectedColorInfo.color}
+                            qty={(orderDetail.qty) ? orderDetail.qty : 1}
+                            address={address}
+                            price={orderDetail.ProductPrice.price}
+                        />;
+                    } else if (orderDetail.part) {
+                        return <PartTable
+                            key={`part${productIndex}`}
+                            type="orderDetails"
+                            productIndex={productIndex}
+                            part={orderDetail.part}
+                            qty={(orderDetail.qty) ? orderDetail.qty : 1}
+                            price={orderDetail.PartPrice.price}
+                        />;
+                    }
+                });
+
+                const buttonSection = (orderStatus == 'Pending') ? <div className="button-container pure-u-1-3">
+                    <div className="btn blue" onClick={() => this.editOrder({ orderId })}>Edit</div>
+                    <div className="btn blue" onClick={() => this.handleAction({ orderId })}>Approve</div>
                 </div>
-                { buttonSection }
-            </div>;
+                    : <div className="button-container pure-u-1-3"></div>;
 
-            let tenantInfoTitle;
-            let tenantInfoDetails;
-            if (order.occupied) {
-                tenantInfoTitle = <tr>
-                    <td><div className="table-header">Tenant Info: </div></td>
-                </tr>;
-                tenantInfoDetails = <tr>
-                    <td><div>{tenantInfo.tenantName} ∙ {tenantInfo.tenantPhoneNumber} ∙ {tenantInfo.tenantEmail}</div></td>
-                </tr>;
+                const detailsHeaderSection = <div className="details-header">
+                    <div className="header-property pure-u-2-3">
+                        <h2 className="property-address">{orderPageHeading.orderNumber}</h2>
+                        <div className="property-manager">{orderPageHeading.address} ● PM Office: {orderPageHeading.PM}</div>
+                    </div>
+                    { buttonSection }
+                </div>;
+
+                let tenantInfoTitle;
+                let tenantInfoDetails;
+                if (order.occupied) {
+                    tenantInfoTitle = <tr>
+                        <td><div className="table-header">Tenant Info: </div></td>
+                    </tr>;
+                    tenantInfoDetails = <tr>
+                        <td><div>{tenantInfo.tenantName} ∙ {tenantInfo.tenantPhoneNumber} ∙ {tenantInfo.tenantEmail}</div></td>
+                    </tr>;
+                } else {
+
+                    tenantInfoTitle = <tr>
+                        <td><div className="table-header">Delivery Contact: </div></td>
+                        <td><div className="table-header">Phone Number: </div></td>
+                    </tr>;
+
+                    tenantInfoDetails = [
+                        <tr>
+                            <td><div>{user.firstName} {user.lastName}</div></td>
+                            <td><div>{user.phoneNumber}</div></td>
+                        </tr>,
+                        <tr>
+                            <td><div>{order.pmOffice.name}</div></td>
+                            <td><div>{order.pmOffice.phoneNumber}</div></td>
+                        </tr>
+                    ];
+                }
+
+                const tenantInfoSection = <div id="admin-table">
+                    <table className="table">
+                        <thead className="head">
+                            { tenantInfoTitle }
+                        </thead>
+                        <tbody>
+                            { tenantInfoDetails }
+                        </tbody>
+                    </table>
+                </div>;
+
+                const orderTotalSection = <div className="cost-section">
+                    <div className="cost-row" ><h4>Sub Total:</h4> ${ order.subTotalCost }</div>
+                    <div className="cost-row" ><h4>Sales Tax:</h4> ${ order.salesTax }</div>
+                    <div className="cost-row" ><h4>Total:</h4> ${ order.totalCost }</div>
+                </div>;
+
+                pageData = <div className="container">
+                    { detailsHeaderSection }
+                    <MyTable
+                        type="orderDetails"
+                        headers={orderHeaders}
+                        data={orderData}
+                    />
+                    { tenantInfoSection }
+                    { productData }
+                    { orderTotalSection }
+                </div>;
             } else {
-
-                tenantInfoTitle = <tr>
-                    <td><div className="table-header">Delivery Contact: </div></td>
-                    <td><div className="table-header">Phone Number: </div></td>
-                </tr>;
-
-                tenantInfoDetails = [
-                    <tr>
-                        <td><div>{user.firstName} {user.lastName}</div></td>
-                        <td><div>{user.phoneNumber}</div></td>
-                    </tr>,
-                    <tr>
-                        <td><div>{order.pmOffice.name}</div></td>
-                        <td><div>{order.pmOffice.phoneNumber}</div></td>
-                    </tr>
-                ];
+                pageData = <Iframe
+                    url={`https://sibi-ge-dev.netlify.com/edit/${this.state.editOrder}`}
+                    width="100%"
+                    height="100%"
+                    allowFullScreen
+                />;
             }
 
-            const tenantInfoSection = <div id="admin-table">
-                <table className="table">
-                    <thead className="head">
-                        { tenantInfoTitle }
-                    </thead>
-                    <tbody>
-                        { tenantInfoDetails }
-                    </tbody>
-                </table>
-            </div>;
 
-            const orderTotalSection = <div className="cost-section">
-                <div className="cost-row" ><h4>Sub Total:</h4> ${ order.subTotalCost }</div>
-                <div className="cost-row" ><h4>Sales Tax:</h4> ${ order.salesTax }</div>
-                <div className="cost-row" ><h4>Total:</h4> ${ order.totalCost }</div>
-            </div>;
-
-            pageData = <div>
-                { detailsHeaderSection }
-                <MyTable
-                    type="orderDetails"
-                    headers={orderHeaders}
-                    data={orderData}
-                />
-                { tenantInfoSection }
-                { productData }
-                { orderTotalSection }
-            </div>
         }
 
         return (
             <div id="orders-page">
-                <div className="container">
-                    { pageData }
-                </div>
+                { pageData }
             </div>
         );
     }
