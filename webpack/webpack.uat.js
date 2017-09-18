7d3e480ca1ccf8c080a034696790dcfc81e8af8a
Seth Bertlshofer <seth@builtbyhq.com>
2017-09-18 09:31:43 -0600

    updated iFrame url based upon the build

--- webpack/webpack.uat.js
+++ webpack/webpack.uat.js
@@ -3,5 +3,6 @@ const WebpackConfig = require('./webpack.config');
 module.exports = WebpackConfig({
     env             : 'development',
     APP_NAME        : 'sibi_ge_admin_uat',
-    API_URL         : 'https://sibi-uat.sibipro.com'
+    API_URL         : 'https://sibi-uat.sibipro.com',
+    ORDER_URL       : 'https://ge-uat.sibipro.com/'
 });
