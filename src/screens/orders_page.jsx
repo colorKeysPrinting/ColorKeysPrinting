3852fcdb5b40f95a68d8080c7936325001ff8ed3
Seth Bertlshofer <seth@builtbyhq.com>
2017-10-16 15:08:16 -0600

    added sort case for manufacture users

--- src/screens/orders_page.jsx
+++ src/screens/orders_page.jsx
@@ -206,17 +206,29 @@ class OrdersPage extends React.Component {
             }
 
             if (sortby.column === '') {
-                data = _.partition(data, ['orderStatus', 'Processed']);
-                data[0] = _.orderBy(data[0], ['createdAt'], ['desc']); // sorts orderStatus pending w/ orderDate
-                data = data[0].concat(data[1]);
+                const re = /manufacturer/;
+                if (re.exec(activeUser.get('type'))) {
+                    data = _.partition(data, ['orderStatus', 'Approved']);
+                    data[0] = _.orderBy(data[0], ['createdAt'], ['desc']); // sorts orderStatus pending w/ orderDate
+                    data = data[0].concat(data[1]);
 
-                data = _.partition(data, ['orderStatus', 'Pending']);
-                data[0] = _.orderBy(data[0], ['createdAt'], ['desc']); // sorts orderStatus pending w/ orderDate
-                data = data[0].concat(data[1]);
+                    data = _.partition(data, ['orderStatus', 'Pending']);
+                    data[0] = _.orderBy(data[0], ['createdAt'], ['desc']); // sorts orderStatus pending w/ orderDate
+                    data = data[1].concat(data[0]);
 
-                data = _.partition(data, ['orderStatus', 'Approved']);
-                data[0] = _.orderBy(data[0], ['createdAt'], ['desc']); // sorts orderStatus pending w/ orderDate
-                data = data[1].concat(data[0]);
+                } else {
+                    data = _.partition(data, ['orderStatus', 'Processed']);
+                    data[0] = _.orderBy(data[0], ['createdAt'], ['desc']); // sorts orderStatus pending w/ orderDate
+                    data = data[0].concat(data[1]);
+
+                    data = _.partition(data, ['orderStatus', 'Pending']);
+                    data[0] = _.orderBy(data[0], ['createdAt'], ['desc']); // sorts orderStatus pending w/ orderDate
+                    data = data[0].concat(data[1]);
+
+                    data = _.partition(data, ['orderStatus', 'Approved']);
+                    data[0] = _.orderBy(data[0], ['createdAt'], ['desc']); // sorts orderStatus pending w/ orderDate
+                    data = data[1].concat(data[0]);
+                }
 
             } else {
                 data = _.orderBy(data, [sortby.column], [sortby.isAsc]);
