41d387f8921ace95afeecf44f5ddda211fa94388
Seth Bertlshofer <celestials2013@gmail.com>
2017-12-09 13:46:50 -0700

    added social media badges and links

--- src/components/footer_bar.jsx
+++ src/components/footer_bar.jsx
@@ -4,6 +4,7 @@ import React                    from 'react';
 import { animateScroll }        from 'react-scroll';
 import { Col, CardPanel, Card, Button } from 'react-materialize';
 import _                        from 'lodash';
+import { ShareButtons, ShareCounts, generateShareIcon } from 'react-share';
 import assets                   from 'utils/assets';
 
 export default class Navbar extends React.Component {
@@ -20,6 +21,15 @@ export default class Navbar extends React.Component {
     }
 
     render(){
+
+        const { FacebookShareButton, GooglePlusShareButton, TwitterShareButton } = ShareButtons;
+
+        const FacebookIcon = generateShareIcon('facebook');
+        const GooglePlusIcon = generateShareIcon('google');
+        const TwitterIcon = generateShareIcon('twitter');
+
+        const title = 'Color Keys Printing';
+
         return (
             <div>
                 <br/>
@@ -27,6 +37,33 @@ export default class Navbar extends React.Component {
                     <Button waves="light" className="waves-effect" onClick={this.scrollToTop}>Back To Top</Button>
                 </div>
                 <footer className="page-footer black">
+                    <div className="social-media-section">
+                        <FacebookShareButton
+                            url={'https://www.facebook.com/colorkeysprinting'}
+                            quote={title}
+                            className="social-media-button">
+                            <FacebookIcon
+                                size={32}
+                                round />
+                        </FacebookShareButton>
+
+                        <TwitterShareButton
+                            url={'https://twitter.com/colorkeys13'}
+                            title={title}
+                            className="social-media-button">
+                            <TwitterIcon
+                                size={32}
+                                round />
+                        </TwitterShareButton>
+
+                        <GooglePlusShareButton
+                            url={'https://plus.google.com/u/0/113724115750329359769'}
+                            className="social-media-button">
+                            <GooglePlusIcon
+                                size={32}
+                                round />
+                        </GooglePlusShareButton>
+                    </div>
                     <div className="container">
                         <div className="row">
                             <Col s={12} m={16} >
